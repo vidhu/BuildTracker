17ca5cb776704191460a22fc7ff42ca45096742f
Sakai <Sakai@SAKURA-Z87.home>
2013-10-15 01:44:57 +0100

    Remove unused import

--- src/uk/co/vidhucraft/buildtracker/BuildTrackerCommandExecuter.java
+++ src/uk/co/vidhucraft/buildtracker/BuildTrackerCommandExecuter.java
@@ -4,7 +4,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class BuildTrackerCommandExecuter implements CommandExecutor {
 	BuildTracker plugin;
